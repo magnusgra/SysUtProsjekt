fccb2007fa80cdeb920d0d5b3b9149a7d2503294
ingrihorten <ingrihor@student.hist.no>
2015-01-07 13:53:10 +0100

    mailting

---
+++ SCRUM/src/main/java/com/team1/proj/mailoppsett/Mail.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Ingrid
+ */
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import prosjekt.Passord;
+
+public class Mail {
+
+    public static void main(String[] args) {
+        //Sendt fra bruker og passord
+        String fra = "nicandsteven@yahoo.com";
+        String passord = "scrumteam1";
+        //Mottaker:
+        String brukernavnet = "ingrid";
+        String til = "ingrid.horten@gmail.com";
+        String host = "smtp.mail.yahoo.com";
+
+        Properties properties = System.getProperties();
+        //Setup mailserver
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.user", fra);
+        properties.put("mail.smtp.password", passord);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            //Lager et default MimeMessage-objekt.
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(fra));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(til));
+
+            //Overskrift
+            message.setSubject("Velkommen som ny bruker.");
+
+            //Melding
+            Passord p = new Passord();
+            String sendt = p.autogenererPassord();
+            message.setText("Er du klar for å spille Nic And Steven og samtidig lære CSS og HTML5?"
+                    + "\n\n Her er det du trenger for å komme igang.\n Brukernavn: "+brukernavnet+" \n Passord: "+sendt+"  \n\n Du kan endre passord på nettsiden etter å ha logget inn");
+
+            //Send meldingen
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, fra, passord);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
---
+++ SCRUM/src/main/java/com/team1/proj/mailoppsett/Passord.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prosjekt;
+
+/**
+ *
+ * @author Ingrid
+ */
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class Passord {
+    private static final Random RANDOM = new SecureRandom();
+    public static final int PASSORD_LENGDE = 8;
+    
+    public static String autogenererPassord(){
+        String bokstaver = "abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String password = "";
+        for(int i=0; i<PASSORD_LENGDE; i++){
+            int index = (int)(RANDOM.nextDouble()*bokstaver.length());
+            password+=bokstaver.substring(index, index+1);
+        }
+        return password;
+    }
+}
+        
+  
+    
+
