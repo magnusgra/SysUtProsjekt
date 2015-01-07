dce17e690f6d9bf224a0bbc6c940d8455e2fad95
arnecs <achrs@hotmail.com>
2015-01-07 09:42:03 +0100

    Controller

---
+++ SCRUM/src/main/java/com/team1/proj/controller/MainController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team1.proj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author arnecs
+ */
+
+
+@Controller
+public class MainController {
+    
+    
+    
+    @RequestMapping(value="/*")
+    public String index(){
+        return "index";
+    }
+    
+}
