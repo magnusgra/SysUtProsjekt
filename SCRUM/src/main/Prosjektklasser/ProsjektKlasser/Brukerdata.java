a5e5ed88f56165ff813dc72105959a9bbda1b94f
mariizz <mari.andrine.t@gmail.com>
2015-01-07 09:25:51 +0100

    Ny PersonData klasse!

---
+++ SCRUM/src/main/Prosjektklasser/ProsjektKlasser/Brukerdata.java
@@ -0,0 +1,47 @@
+package ProsjektKlasser;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mari
+ */
+public class Brukerdata {
+    private String email; 
+    private String userName; 
+    private String password; 
+
+    public Brukerdata(String email, String password){
+        this.email; 
+        this.password; 
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
