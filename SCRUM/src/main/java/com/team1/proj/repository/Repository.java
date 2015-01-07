65bad621ac549facd95b332937ad4680217d44ee
arnecs <achrs@hotmail.com>
2015-01-07 10:22:10 +0100

    Ropositoryinterface lagt til

---
+++ SCRUM/src/main/java/com/team1/proj/repository/Repository.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team1.proj.repository;
+
+/**
+ *
+ * @author arnecs
+ */
+public interface Repository {
+    
+  //  public void leggTilBruker(Brukerdata bd);
+}
