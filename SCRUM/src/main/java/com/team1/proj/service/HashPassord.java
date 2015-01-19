7ca2d6d83f18f0e779e81ca30ad84173d6b2bdc9
mariizz <mari.andrine.t@gmail.com>
2015-01-19 13:59:22 +0100

    hash

--- SCRUM/src/main/java/com/team1/proj/service/HashPassord.java
+++ SCRUM/src/main/java/com/team1/proj/service/HashPassord.java
@@ -13,150 +13,74 @@ import java.security.SecureRandom;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.SecretKeyFactory;
 import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import javax.mail.NoSuchProviderException;
 
 
  // PBKDF2 saltet passord hashing.
  
 public class HashPassord {
-    public static final String PBKDF2_ALGORITME = "PBKDF2WithHmacSHA1";
-
-    public static final int SALT_BYTE_STR = 24;
-    public static final int HASH_BYTE_STR = 24;
-    public static final int PBKDF2_KJORING = 1000;
-
-    public static final int KJORE_INDEKS = 0;
-    public static final int SALT_INDEKS = 1;
-    public static final int PBKDF2_INDEKS = 2;
-
-    /*
-     * Returnerer et saltet PBKDF2-hash av passordet.
-     */
-    public static String lagHash(String passord) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return lagHash(passord.toCharArray());
-    }
-
-    public static String lagHash(char[] passord) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        // Genererer et random salt
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[SALT_BYTE_STR];
-        random.nextBytes(salt);
-
-        // Hasher passorder
-        byte[] hash = pbkdf2(passord, salt, PBKDF2_KJORING, HASH_BYTE_STR);
-        // formaterer kjøring:salt:hash
-        return PBKDF2_KJORING + ":" + toHex(salt) + ":" +  toHex(hash);
-    }
-
-    /*
-     * Validerer et passord som bruker hash.
-     */
-    public static boolean validerPassord(String passord, String riktigHash) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return validerPassord(passord.toCharArray(), riktigHash);
-    }
-
-    public static boolean validerPassord(char[] passord, String riktigHash) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        // Dekoder hashen til dens parametre
-        String[] params = riktigHash.split(":");
-        int iterations = Integer.parseInt(params[KJORE_INDEKS]);
-        byte[] salt = fromHex(params[SALT_INDEKS]);
-        byte[] hash = fromHex(params[PBKDF2_INDEKS]);
-        byte[] testHash = pbkdf2(passord, salt, iterations, hash.length);
-        return slowEquals(hash, testHash);
-    }
-
-    /**
-     * SAmmenlikner to byte tabeller i lengde-tid. Metoden brukes
-     * slik at passordhasher ikke kan tas ut fra et online system
-     * ved å bruke et "timing attack" å følgende attacke offline
-     */
-    private static boolean slowEquals(byte[] a, byte[] b)
-    {
-        int diff = a.length ^ b.length;
-        for(int i = 0; i < a.length && i < b.length; i++)
-            diff |= a[i] ^ b[i];
-        return diff == 0;
-    }
-
-    /**
-     *  Beregner PBKDF2 hashen til et passord.
-     */
-    private static byte[] pbkdf2(char[] passord, byte[] salt, int iterations, int bytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        PBEKeySpec spec = new PBEKeySpec(passord, salt, iterations, bytes * 8);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITME);
-        return skf.generateSecret(spec).getEncoded();
-    }
-
-    /**
-     * Konverterer en string av hexadesimal characters til en byte tabell
-     */
-    private static byte[] fromHex(String hex)
-    {
-        byte[] binary = new byte[hex.length() / 2];
-        for(int i = 0; i < binary.length; i++)
-        {
-            binary[i] = (byte)Integer.parseInt(hex.substring(2*i, 2*i+2), 16);
+    public static void main(String[] args) {
+        String passwordToHash = "password";
+        String salt = null;
+        try{
+            salt = HashPassord.getSalt();
+        }catch(NoSuchAlgorithmException | NoSuchProviderException | java.security.NoSuchProviderException e){
+            e.printStackTrace();
         }
-        return binary;
+        String securePassword = getSecurePassword(passwordToHash, salt);
+        String sikker = securePassword+":"+salt; 
+        System.out.println(securePassword + ":" + salt); //Prints 83ee5baeea20b6c21635e4ea67847f66
+        String[] si = sikker.split(":");
+        System.out.println("Splitt : "+ si[0]+":"+si[1]);
+         
+        String regeneratedPassowrdToVerify = getSecurePassword(passwordToHash, salt);
+        System.out.println(regeneratedPassowrdToVerify + ":" + salt); //Prints 83ee5baeea20b6c21635e4ea67847f66
     }
-
-    /**
-     * Konverterer en byte tabell til en  hexadesimal string.
-     */
-    private static String toHex(byte[] array)
-    {
-        BigInteger bi = new BigInteger(1, array);
-        String hex = bi.toString(16);
-        int paddingLength = (array.length * 2) - hex.length();
-        if(paddingLength > 0) 
-            return String.format("%0" + paddingLength + "d", 0) + hex;
-        else
-            return hex;
-    }
-
-    /**
-     * Tester
-     */
-    public static void main(String[] args)
-    {
-        try
-        {
-            // Printer ut 10 hasher
-            for(int i = 0; i < 10; i++)
-                System.out.println(HashPassord.lagHash("p\r\nassw0Rd!"));
-
-            // Tester passord validering
-            boolean failure = false;
-            System.out.println("Kjører tester...");
-            for(int i = 0; i < 100; i++)
+   // public static String hashPassord(String passord) throws NoSuchAlgorithmException, NoSuchProviderException, java.security.NoSuchProviderException{
+   //     String salt = HashPassord.getSalt(); 
+    //    return HashPassord.getSecurePassword(passord, salt);
+        
+    //}
+     
+    public static String getSecurePassword(String passwordToHash, String salt){
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(salt.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
             {
-                String passord = ""+i;
-                String hash = lagHash(passord);
-                String andreHash = lagHash(passord);
-                if(hash.equals(andreHash)) {
-                    System.out.println("FEIL: TO HASHER ER LIKE!");
-                    failure = true;
-                }
-                String feilPassword = ""+(i+1);
-                if(validerPassord(feilPassword, hash)) {
-                    System.out.println("FEIL: FEIL PASSORD AKSEPTERT!");
-                    failure = true;
-                }
-                if(!validerPassord(passord, hash)) {
-                    System.out.println("FEIL: GYLDIG PASSORD IKKE AKSEPTERT!");
-                    failure = true;
-                }
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
             }
-            if(failure)
-                System.out.println("TEST FEILET!");
-            else
-                System.out.println("TEST OK!");
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
         }
-        catch(Exception ex)
-        {
-            System.out.println("ERROR: " + ex);
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
+        return generatedPassword;
+    }
+     
+    //Add salt
+    public static String getSalt() throws NoSuchAlgorithmException, NoSuchProviderException, java.security.NoSuchProviderException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        //Create array for salt
+        byte[] salt = new byte[16];
+        //Get a random salt
+        sr.nextBytes(salt);
+        //return salt
+        return salt.toString();
     }
 
 }
