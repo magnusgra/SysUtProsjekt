c729ae91699117d2c798c72d59ea7aa7fa7bbf4c
mariizz <mari.andrine.t@gmail.com>
2015-01-20 12:33:40 +0100

    test

--- SCRUM/src/main/java/com/team1/proj/service/HashPassord.java
+++ SCRUM/src/main/java/com/team1/proj/service/HashPassord.java
@@ -32,12 +32,12 @@ public class HashPassord {
         }
         String securePassword = getSecurePassword(passwordToHash, salt);
         String sikker = securePassword+":"+salt; 
-        System.out.println(securePassword + ":" + salt); //Prints 83ee5baeea20b6c21635e4ea67847f66
+        System.out.println(securePassword + ":" + salt); //skriver 83ee5baeea20b6c21635e4ea67847f66
         String[] si = sikker.split(":");
         System.out.println("Splitt : "+ si[0]+":"+si[1]);
          
         String regeneratedPassowrdToVerify = getSecurePassword(passwordToHash, salt);
-        System.out.println(regeneratedPassowrdToVerify + ":" + salt); //Prints 83ee5baeea20b6c21635e4ea67847f66
+        System.out.println(regeneratedPassowrdToVerify + ":" + salt); //skriver 83ee5baeea20b6c21635e4ea67847f66
     }
    // public static String hashPassord(String passord) throws NoSuchAlgorithmException, NoSuchProviderException, java.security.NoSuchProviderException{
    //     String salt = HashPassord.getSalt(); 
@@ -46,40 +46,39 @@ public class HashPassord {
     //}
      
     public static String getSecurePassword(String passwordToHash, String salt){
-        String generatedPassword = null;
+        String generertPassord = null;
         try {
-            // Create MessageDigest instance for MD5
+            // Lager MessageDigest instanse for MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
+            //Legger til passord-bytes til digest
             md.update(salt.getBytes());
-            //Get the hash's bytes
+            //Henter bytes fra hashen
             byte[] bytes = md.digest(passwordToHash.getBytes());
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
+            // bytes[] har bytes på desimalformat;
+            //Konverterer det til hexadesimalt format
             StringBuilder sb = new StringBuilder();
             for(int i=0; i< bytes.length ;i++)
             {
                 sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
             }
-            //Get complete hashed password in hex format
-            generatedPassword = sb.toString();
+            //lager det hashede passordet i hex-format
+            generertPassord = sb.toString();
         }
         catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        return generatedPassword;
+        return generertPassord;
     }
-     
-    //Add salt
+    
     public static String getSalt() throws NoSuchAlgorithmException, NoSuchProviderException, java.security.NoSuchProviderException
     {
-        //Always use a SecureRandom generator
+        //Bruker SecureRandom generator
         SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
-        //Create array for salt
+        //lager en byteliste med saltet
         byte[] salt = new byte[16];
-        //Get a random salt
+        //henter tilfeldig salt
         sr.nextBytes(salt);
-        //return salt
+        //returnerer salt
         return salt.toString();
     }
 
